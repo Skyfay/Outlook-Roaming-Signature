424ee05923e192dfa02736d2e1b7f6d386d81e28
Manu <76796007+Skyfay@users.noreply.github.com>
2022-11-21 12:13:42 +0100

    Create roaming-signature-setup.ps1

---
+++ roaming-signature-setup.ps1
@@ -0,0 +1,43 @@
+# creator: Skyfay
+# Support: support@skyfay.ch
+# Last edit: 21.11.2022
+
+#‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾#
+#->                                                                       Funcions                                                              <-#
+#_________________________________________________________________________________________________________________________________________________#
+
+function disable_roaming_signature {
+    $path1 = 'HKCU:\SOFTWARE\Microsoft\Office\16.0\Outlook\Setup'
+    New-ItemProperty -Path $path1 -Name 'DisableRoamingSignaturesTemporaryToggle' -Value 1 -PropertyType DWord
+
+}
+
+function enable_roaming_signature {
+    $path1 = 'HKCU:\SOFTWARE\Microsoft\Office\16.0\Outlook\Setup'
+    Remove-ItemProperty -Path $path1 -Name 'DisableRoamingSignaturesTemporaryToggle'
+}
+
+#‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾#
+#->                                                                     Main Code                                                               <-#
+#_________________________________________________________________________________________________________________________________________________#
+
+cls
+Write-Host "With this script you can disable and enable roaming signatures for Outlook."
+Start-Sleep 1
+Write-Host `n
+Write-Host "(1)  -  disable roaming signatures"
+Write-Host "(2)  -  enable roaming signatures"
+Write-Host `n
+
+$value = Read-Host "Do you want to enable or disable roaming signatures?"
+
+switch ($value) {
+    1 {disable_roaming_signature}
+    2 {enable_roaming_signature}
+    Default {"Please enter a value between 1-2"}
+}
+
+cls
+
+Write-Host "You have successfully reconfigured roaming signatures for Outlook"
+Start-Sleep 1
